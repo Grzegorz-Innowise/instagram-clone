20366a963ab3e0e75e64bbc37d66dbbd7e095145
Grzegorz Strzeszewski <grzegorz.strzeszewski@innowise.com>
2025-04-16 01:15:13 +0200

    feat(86c32d3ft): Implemented global exception filters and reqest logging.

---
+++ packages/backend/src/logger/logger.middleware.ts
@@ -0,0 +1,17 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { LoggerService } from './logger.service';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  constructor(private readonly logger: LoggerService) {}
+
+  use(req: Request, res: Response, next: NextFunction) {
+    const { method, originalUrl, ip } = req;
+    const logMessage = `Request - ${method} ${originalUrl} from ${ip}`;
+
+    this.logger.log(logMessage, 'RequestLogger');
+
+    next();
+  }
+}
